ac6b02d091151b8b27e6048e723031fac1f47eba
gnoey048 <d0day0400@gmail.com>
2021-02-20 20:59:58 +0900

    Update todolist.js

--- js/todolist.js
+++ js/todolist.js
@@ -4,17 +4,55 @@ const toDoForm = document.querySelector(".to-do-form");
 const inputToDo = toDoForm.querySelector("input");
 
 const TODOS_LS = "ToDos";
+const COMPLETE_LS = "complete";
 
-function saveToDoInLocal(text) {
-  localStorage.setItem(TODOS_LS, text);
+const toDos = [];
+const complete = [];
+
+function saveToDoInLocal() {
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+  localStorage.setItem(COMPLETE_LS, JSON.stringify(complete));
 }
 
 function handleSubmit(event) {
   event.preventDefault();
   const currentValue = inputToDo.value;
-  saveToDoInLocal(currentValue);
+  showingToDo(currentValue);
+  inputToDo.value = "";
 }
 
-function showingToDo() {}
-
+function showingToDo(text) {
+  const list = document.createElement("li");
+  const content = document.createElement("span");
+  const delBtn = document.createElement("button");
+  const completeBtn = document.createElement("button");
+  const newId = toDos.length + 1;
+  content.innerText = text;
+  delBtn.innerText = "❌";
+  completeBtn.innerText = "☑️";
+  list.appendChild(content);
+  list.appendChild(delBtn);
+  list.appendChild(completeBtn);
+  list.id = newId;
+  toDoList.appendChild(list);
+  const toDoObj = {
+    text: text,
+    id: newId,
+  };
+  toDos.push(toDoObj);
+  saveToDoInLocal();
+}
+function parsedToDo() {
+  const loadedToDo = localStorage.getItem(TODOS_LS);
+  const loadedComplete = localStorage.getItem(COMPLETE_LS);
+  const parsedToDo = JSON.parse(loadedToDo);
+  const parsedComplete = JSON.parse(loadedComplete);
+  parsedToDo.forEach((toDo) => {
+    showingToDo(toDo.text);
+  });
+  parsedComplete.forEach((toDo) => {
+    //showingComplete(toDo.text);
+  });
+}
+parsedToDo();
 toDoForm.addEventListener("submit", handleSubmit);
