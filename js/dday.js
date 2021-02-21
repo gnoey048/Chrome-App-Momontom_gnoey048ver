43be99fe582d754a8af32d9ad5c9f6d67968d207
gnoey048 <d0day0400@gmail.com>
2021-02-21 20:24:53 +0900

    update dday and weather.js

--- js/dday.js
+++ js/dday.js
@@ -9,7 +9,6 @@ function getTime() {
     (ddayTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
   );
   const minutes = Math.floor((ddayTime % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((ddayTime % (1000 * 60)) / 1000);
   D_DAY.innerText = `${days < 10 ? `0${days}` : days}d ${
     hours < 10 ? `0${hours}` : hours
   }h ${minutes < 10 ? `0${minutes}` : minutes}m `;
